9df0cf4b33aae290e585d1fd300cfb79ab5f3ab3
Dio-Damar-Danendra-Portofolio <diodamar14102000@gmail.com>
2025-04-27 22:39:58 +0700

    Update Database

--- praujikom_pos.sql
+++ praujikom_pos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 25, 2025 at 10:28 AM
+-- Generation Time: Apr 27, 2025 at 05:38 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -41,7 +41,9 @@ INSERT INTO `categories` (`id`, `name`) VALUES
 (2, 'Makanan Ringan'),
 (3, 'Minuman Manis'),
 (4, 'Makanan Penutup'),
-(5, 'Minuman Soda');
+(5, 'Minuman Soda'),
+(6, 'Minuman Isotonik'),
+(7, 'Air Mineral');
 
 -- --------------------------------------------------------
 
@@ -85,17 +87,22 @@ CREATE TABLE `products` (
   `category_id` int(255) NOT NULL,
   `price` decimal(10,2) NOT NULL,
   `image` varchar(255) DEFAULT NULL,
-  `options` int(11) DEFAULT NULL
+  `options` int(11) DEFAULT NULL,
+  `is_available` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `category_id`, `price`, `image`, `options`) VALUES
-(1, 'Lunpia', 1, 15000.00, 'Lunpia.jpg', NULL),
-(2, 'Ayam Goreng Laos', 1, 30000.00, 'Ayam Goreng Laos.jpg', NULL),
-(3, 'Donat', 1, 12000.00, 'Donat Stroberi.png', NULL);
+INSERT INTO `products` (`id`, `name`, `category_id`, `price`, `image`, `options`, `is_available`) VALUES
+(1, 'Lunpia', 1, 15000.00, 'Lunpia.jpg', NULL, 1),
+(2, 'Ayam Goreng Laos', 1, 30000.00, 'Ayam Goreng Laos.jpg', NULL, 1),
+(3, 'Donat', 4, 12000.00, 'Donat Stroberi.png', NULL, 0),
+(4, 'Jus Apel', 3, 11500.00, 'Jus apel.jpg', NULL, 1),
+(5, 'Es Krim Cokelat', 4, 16000.00, 'Es Krim Cokelat.jpg', NULL, 0),
+(6, 'Coca-Cola', 5, 5000.00, 'Coca-Cola.jpg', NULL, 1),
+(7, 'Aqua', 7, 8000.00, 'Aqua.jpg', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -123,19 +130,19 @@ CREATE TABLE `users` (
   `role` varchar(255) NOT NULL,
   `profile_picture` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `remember` tinyint(4) DEFAULT NULL
+  `phone_number` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `profile_picture`, `created_at`, `remember`) VALUES
-(1, 'Dio Damar Danendra', 'diodamar14102000@gmail.com', 'b!Nu$!@N202350C5', 'Admin', NULL, '2025-04-24 04:06:48', NULL),
-(2, 'Gendis Ayu', 'gendisayu@gmail.com', 'k40$Fr0z3n', 'Kasir', NULL, '2025-04-24 04:08:52', NULL),
-(3, 'Faiha Wanda Nabilah', 'faihafaiha.email@gmail.com', 'c@r!p0k3m0N', 'Kasir', NULL, '2025-04-25 01:11:48', NULL),
-(4, 'Leza Arlan', 'leza_arlan@gmail.com', 'k@!C0mmut3r', 'Pimpinan', NULL, '2025-04-25 01:20:39', NULL),
-(5, 'Nabila Anggraini', 'nabillanggrainiii@gmail.com', 'uNp@M04031999', 'Kasir', NULL, '2025-04-25 01:23:06', NULL);
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `profile_picture`, `created_at`, `phone_number`) VALUES
+(1, 'Dio Damar Danendra', 'diodamar14102000@gmail.com', 'b!Nu$!@N202350C5', 'Admin', 'DIO DAMAR DANENDRA_Blue.jpg', '2025-04-24 04:06:48', '085772111179'),
+(2, 'Gendis Ayu', 'gendisayu@gmail.com', 'k40$Fr0z3n', 'Kasir', 'Elsa.png', '2025-04-24 04:08:52', '087871095885'),
+(3, 'Faiha Wanda Nabilah', 'faihafaiha.email@gmail.com', 'c@r!p0k3m0N', 'Kasir', 'Pokemon.jpg', '2025-04-25 01:11:48', '0818718067'),
+(4, 'Leza Arlan', 'leza_arlan@gmail.com', 'k@!C0mmut3r', 'Pimpinan', 'KRL JR East seri 205.jpg', '2025-04-25 01:20:39', '087851695345'),
+(5, 'Nabila Anggraini', 'nabillanggrainiii@gmail.com', 'uNp@M04031999', 'Kasir', 'Test_PraUjikom.jpg', '2025-04-25 01:23:06', '08271598367');
 
 --
 -- Indexes for dumped tables
@@ -190,7 +197,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `orders`
@@ -202,7 +209,7 @@ ALTER TABLE `orders`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `top_selling_products`
