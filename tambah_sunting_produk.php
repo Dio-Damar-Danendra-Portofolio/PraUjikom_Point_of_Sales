a2572bd0c362f2d5dbd19791bc2c477960d34260
Dio-Damar-Danendra-Portofolio <diodamar14102000@gmail.com>
2025-04-28 15:49:39 +0700

    Update Aplikasi

--- tambah_sunting_produk.php
+++ tambah_sunting_produk.php
@@ -89,7 +89,7 @@
                 <div class="row bg-danger justify-content-left">
                     <div class="col-lg-12">
                         <div class="text-white text-left">
-                            <h1><?php echo isset($_GET['id-produk']) ? 'Ubah Data ' : 'Tambah '?> Produk</h1>
+                            <h1><?php echo isset($_GET['id-produk']) ? 'Ubah Data' : 'Tambah'?> Produk</h1>
                         </div>
                     </div>
                 </div>
